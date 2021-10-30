4415ed9fe4d70afb516a800acada7f9cdf102636
Bojan Karlaš <bojan.karlas@gmail.com>
2021-10-30 18:04:56 +0200

    Remove Lang reference.

--- src/extension.js
+++ src/extension.js
@@ -6,7 +6,6 @@ const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 
 async function execCommand(argv, cancellable=null) {
